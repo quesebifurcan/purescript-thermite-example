3c0bd774c1c285b4960e11c5139e7f0b4656b4a0
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2017-06-24 11:32:25 +0200

    2017-06-11: Lenses, hot reloading and `Unsafe.Coerce`

--- src/Lesson2/Counter.purs
+++ src/Lesson2/Counter.purs
@@ -1,24 +1,32 @@
 module Lesson2.Counter
   ( counter
+  , count
   , initialState
   , CounterAction(..)
   ) where
 
 import Prelude
 import React.DOM as RD
+import React.DOM.Props as RP
 import Thermite as T
-import Data.Lens (Prism', prism', Lens, lens, addOver, subOver, _1, _2)
+import Data.Lens (Prism', prism', Lens, lens, over, view, set, addOver, subOver, _1, _2)
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
 import Lesson2.Button (ButtonAction(..), ButtonState, button)
+import Unsafe.Coerce -- Here is the culprit
 
 type CounterState =
   { count :: Int
   , buttonState :: Tuple ButtonState ButtonState
+  , incrementBy :: Int
   }
 
 count :: forall a b r. Lens { count :: a | r } { count :: b | r } a b
-count = lens _.count (_ { count = _ })
+-- count = lens _.count (_ { count = _ })
+count = lens (\x -> x.count) (\x newValue -> x { count = newValue })
+
+incrementBy :: forall a b r. Lens { incrementBy :: a | r } { incrementBy :: b | r } a b
+incrementBy = lens _.incrementBy (_ { incrementBy = _ })
 
 buttonState :: forall a b r. Lens { buttonState :: a | r } { buttonState :: b | r } a b
 buttonState = lens _.buttonState (_ { buttonState = _ })
@@ -33,23 +41,49 @@ initialState =
     { text: "Decrement"
     , className: "btn btn-danger"
     }
+  , incrementBy: 2
   }
 
 data CounterAction
   = Increment
   | Decrement
+  | SetIncrementBy Int
 
 render :: T.Render CounterState _ _
-render _ _ state _ =
+render dispatch _ state _ =
   [ RD.h1' [ RD.text "Lesson 2 - Separating State" ]
   , RD.p'  [ RD.text "The state is: "
            , RD.text (show state.count)
            ]
+  , RD.input [
+       RP.onChange (\x -> dispatch (SetIncrementBy (unsafeCoerce x).target.value))
+       , RP.value (show state.incrementBy)
+       ] []
   ]
 
+-- The simple version
+-- performIncrementBy state =
+--   state { count = state.count + state.incrementBy }
+
+-- performIncrementBy ::
+--   forall s t a b s' t' a' b' .
+--   Lens s t a b ->
+--   Lens s' t' a' b' ->
+--   CounterState ->
+--   CounterState
+
+-- See https://stackoverflow.com/questions/20626963/haskell-lenses-getters-and-setters
+performIncrementBy countL incrementByL countL' state =
+  let incrementBy = view incrementByL state
+      count = view countL state
+  in
+   set countL' (count + incrementBy) state
+
 performAction :: T.PerformAction _ CounterState _ CounterAction
-performAction Increment _ _ = void $ T.modifyState $ addOver count 1
+-- performAction Increment _ _ = void $ T.modifyState $ (over count id)
+performAction Increment _ _ = void $ T.modifyState $ performIncrementBy count incrementBy count
 performAction Decrement _ _ = void $ T.modifyState $ subOver count 1
+performAction (SetIncrementBy x) _ _ = void $ T.modifyState $ (over incrementBy (\_ -> x))
 
 display :: T.Spec _ CounterState _ CounterAction
 display = T.simpleSpec performAction render
