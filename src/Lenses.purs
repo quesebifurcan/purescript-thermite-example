a346da7c0122fc734f8ff4176e235f19f4462508
Daniel Bachler <daniel@danielbachler.de>
2017-07-20 21:31:14 +0200

    Added example for appending to nested list

--- src/Lenses.purs
+++ src/Lenses.purs
@@ -14,7 +14,13 @@ derive instance streetNewtype :: Newtype Street _
 type Address =
   { street :: Street
   , number :: Int
+  , additionalLines :: Array String
 }
+
+type Person =
+  { name :: String
+  , addresses :: Array Address
+  }
 
 _streetIso :: Iso Street Street String String
 _streetIso = _Newtype
@@ -22,6 +28,8 @@ _streetIso = _Newtype
 _street :: Lens' Address Street
 _street = prop (SProxy :: SProxy "street")
 
+_additionalLines :: Lens' Address (Array String)
+_additionalLines = prop (SProxy :: SProxy "additionalLines")
 
 _number :: Lens' Address Int
 _number = prop (SProxy :: SProxy "number")
@@ -30,7 +38,7 @@ _streetDirect :: Lens' Address String
 _streetDirect = _street <<< _streetIso
 
 addressVal :: Address
-addressVal = { street: Street "test", number: 15 }
+addressVal = { street: Street "test", number: 15, additionalLines: [] }
 
 streetVal :: String
 streetVal = addressVal ^. _street ^. _streetIso
@@ -38,3 +46,7 @@ streetVal = addressVal ^. _street ^. _streetIso
 setNumber :: Address -> Int -> Address
 setNumber address newNum =
   address # _number .~ newNum
+
+addLine :: Address -> String -> Address
+addLine address line =
+  address # _additionalLines <>~ [line]
