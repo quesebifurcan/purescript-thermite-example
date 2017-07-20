235d249fef239c1e8a039b81b739c55e09925c7b
Daniel Bachler <daniel@danielbachler.de>
2017-07-20 14:25:23 +0200

    Added newtype for street with getter and first setter. The setter doesn't work inline though?!

--- src/Lenses.purs
+++ src/Lenses.purs
@@ -1,18 +1,40 @@
 module Lenses where
 
+import Prelude
 import Data.Lens
 import Data.Lens.Record
 import Data.Symbol
+import Data.Lens.Iso.Newtype
+import Data.Newtype
+import Control.Bind ((=<<))
+
+newtype Street = Street String
+derive instance streetNewtype :: Newtype Street _
 
 type Address =
-  { street :: String
+  { street :: Street
   , number :: Int
 }
 
-_street :: Lens' Address String
+_streetIso :: Iso Street Street String String
+_streetIso = _Newtype
+
+_street :: Lens' Address Street
 _street = prop (SProxy :: SProxy "street")
 
-addressVal :: Address
-addressVal = { street: "test", number: 15 }
 
-streetVal = addressVal ^. _street
+_number :: Lens' Address Int
+_number = prop (SProxy :: SProxy "number")
+
+_streetDirect :: Lens' Address String
+_streetDirect = _street <<< _streetIso
+
+addressVal :: Address
+addressVal = { street: Street "test", number: 15 }
+
+streetVal :: String
+streetVal = addressVal ^. _street ^. _streetIso
+
+setNumber :: Address -> Int -> Address
+setNumber address newNum =
+  (.~) _number newNum address
